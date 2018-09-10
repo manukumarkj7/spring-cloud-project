c4ec07a35884765c60773f2a43e56b321c41349d
manukumarkj7 <42763661+manukumarkj7@users.noreply.github.com>
2018-09-10 14:10:31 +0530

    Add files via upload

---
+++ spring-boot-microservice-currency-conversion-service/src/main/java/com/in28minutes/springboot/microservice/example/currencyconversion/SpringBootMicroserviceCurrencyConversionApplication.java
@@ -0,0 +1,19 @@
+package com.in28minutes.springboot.microservice.example.currencyconversion;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@org.springframework.cloud.openfeign.EnableFeignClients("com.in28minutes.springboot.microservice.example.currencyconversion")
+@EnableDiscoveryClient
+public class SpringBootMicroserviceCurrencyConversionApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootMicroserviceCurrencyConversionApplication.class, args);
+  }
+}
